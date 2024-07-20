5a57a639e80818458c47842d45d735e7d58aa0ae
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-20 16:25:00 +0000

    Created movie header

---
+++ app/views/templates/headerMovie.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link rel="stylesheet" href="/css/styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="icon" href="/favicon.png">
+  <title>Movie Search & Movie Rating</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/movies">Movie Search</a>
+      <div class="navbar-nav">
+        <a class="nav-link" href="/movies">Search</a>
+        <a class="nav-link" href="/movies/ratings">Your Ratings</a>
+      </div>
+    </div>
+</nav>
+<main class="container">
