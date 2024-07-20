7d21ca66a82e7548caed8e637b35aa412b551006
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-20 17:15:12 +0000

    Created rating.php model

---
+++ app/models/Rating.php
@@ -0,0 +1,15 @@
+<?php
+class Rating {
+  public function saveRating($userId, $movieName, $rating) {
+    $db = db_connect();
+    $statement = $db->prepare("insert into ratings (user_id, movie_name, rating) values (:userId, :movieName, :rating)");
+    $statement->execute();
+  }
+  public function getUserRatings($userId) {
+    $db = db_connect();
+    $statement = $db->prepare("select * from ratings where user_id = :userId");
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+}
