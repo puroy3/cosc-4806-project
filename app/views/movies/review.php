b804143bdb08799d4a05a3322defec7b703ffb88
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-21 13:27:06 +0000

    Created review view

---
+++ app/views/movies/review.php
@@ -0,0 +1,9 @@
+<?php require_once 'app/views/templates/headerMovie.php'?>
+<h1>Review for <?= htmlspecialchars($data['movieName']) ?></h1>
+<div class="card">
+  <div class="card-body">
+    <?= n12br(htmlspecialchars($data['review']))?>
+</div>
+</div>
+<a href="/movies" class="btn btn-dark">Return to Search</a>
+<?php require_once 'app/views/templates/footer.php'?>
