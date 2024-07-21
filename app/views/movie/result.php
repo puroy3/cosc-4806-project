802c5338e55ae7208e4d6fb47aab8410e958ed8d
puroy3 <168604388+puroy3@users.noreply.github.com>
2024-07-21 20:46:42 +0000

    Display Ratings provided by online sources such as Metacritic

--- app/views/movie/result.php
+++ app/views/movie/result.php
@@ -1,10 +1,34 @@
 <?php require_once 'app/views/templates/headerMovie.php'?>
 <?php if (isset($data['movie']) && is_array($data['movie'])): ?>
-<h1><?= htmlspecialchars($data['movie']['Title'])?></h1>
-<img src="<?= htmlspecialchars($data['movie']['Poster'])?>" alt="Movie Poster" class="img-fluid">
-<p>Year: <?= htmlspecialchars($data['movie']['Year'])?></p>
-<p>Director: <?= htmlspecialchars($data['movie']['Director'])?></p>
-<p>Plot Summary: <?= htmlspecialchars($data['movie']['Plot'])?></p>
+<h1><?= htmlspecialchars($data['movie']['Title'])?> (<?= htmlspecialchars($data['movie']['Year']) ?>)</h1>
+<div class="row">
+  <div class="col-md-3">
+    <img src="<?= htmlspecialchars($data['movie']['Poster'])?>" alt="Movie Poster" class="img-fluid">
+</div>
+  <div class="col-md-7">
+    <p>Year: <?= htmlspecialchars($data['movie']['Year'])?></p>
+    <p>Released:</p> <?= htmlspecialchars($data['movie']['Released'])?></p>
+    <p>Director: <?= htmlspecialchars($data['movie']['Director'])?></p>
+    <p>Writer:</p> <?= htmlspecialchars($data['movie']['Writer'])?></p>
+    <p>Actors:</p> <?= htmlspecialchars($data['movie']['Actors'])?></p>
+    <p>Rated:</p> <?= htmlspecialchars($data['movie']['Rated'])?></p>
+    <p>Runtime:</p> <?= htmlspecialchars($data['movie']['Runtime'])?></p
+    <p>Genre:</p> <?= htmlspecialchars($data['movie']['Genre'])?></p>
+    <p>Countries:</p> <?= htmlspecialchars($data['movie']['Country'])?></p>
+    <p>Language:</p> <?= htmlspecialchars($data['movie']['Language'])?></p>
+    <p>Awards:</p> <?= htmlspecialchars($data['movie']['Awards'])?></p>
+    <p>Box Office: <?= htmlspecialchars($data['movie']['BoxOffice']) ?></p>
+    <p>Plot Summary: <?= htmlspecialchars($data['movie']['Plot'])?></p>
+    <h3>Ratings</h3>
+    <?php if (isset($data['movie']['Ratings']) && is_array($data['movie']['Ratings'])): ?>
+      <ul>
+        <?php foreach ($data['movie']['Ratings'] as $rating): ?>
+          <li><?= htmlspecialchars($rating['Source']) ?>: <?= htmlspecialchars($rating['Value']) ?></li>
+        <?php endforeach; ?>
+      </ul>
+  <?php endif; ?>
+  </div>
+</div>
 <?php if (isset($_SESSION['auth'])): ?>
 <h2>Rate this movie</h2>
 <form action="/movie/rate" method="POST">
@@ -17,13 +41,11 @@
         <option value="4">4 Stars</option>
         <option value="5">5 Stars</option>
       </select>
+  <br>
   <button type="submit" class="btn btn-dark">Submit Rating</button>
 </form>
 <?php else: ?>
   <p><a href="/login">Login</a> to rate movies.</p>
-<?php endif; ?>
-<?php else: ?>
-  <p>No data is available for this movie.</p>
 <?php endif; ?>
 <br>
 <h2>Get an AI-generated Review</h2>
@@ -43,4 +65,7 @@
     <br>
     <button type="submit" class="btn btn-dark">Get a Review</button>
 </form>
+  <?php else: ?>
+    <p>No data is available for this movie.</p>
+  <?php endif; ?>
 <?php require_once 'app/views/templates/footer.php'?>
